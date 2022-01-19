2fa81ab231283ece6103737e4d7ef479c4a15958
mahesh.takale <mahesh8497>
2022-01-19 11:01:27 +0530

    controller: added controller

--- src/test/java/com/cicd/cicdjenkinsdemo/CicdJenkinsDemoApplicationTests.java
+++ src/test/java/com/cicd/cicdjenkinsdemo/CicdJenkinsDemoApplicationTests.java
@@ -1,10 +1,15 @@
 package com.cicd.cicdjenkinsdemo;
 
+import com.cicd.cicdjenkinsdemo.controller.SmsController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CicdJenkinsDemoApplicationTests {
+
+	@Autowired
+	private SmsController controller;
 
 	@Test
 	void contextLoads() {
